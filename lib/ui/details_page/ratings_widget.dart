e0dc0bfc75765d83143a21ba8819ec524b32a843
Serko Katsikian <skatsikian@zipcar.co.uk>
2018-07-19 01:21:58 +0100

    - add bigger rotten tomatoe image

--- lib/ui/details_page/ratings_widget.dart
+++ lib/ui/details_page/ratings_widget.dart
@@ -50,7 +50,7 @@ class RatingsWidget extends StatelessWidget {
 	Widget buildRottenTomatoesRating() {
 		return Row(
 			children: <Widget>[
-				IconButton(iconSize: 30.0, icon: Image(image: AssetImage
+				IconButton(icon: Image(image: AssetImage
 					("assets/rotten_tomatoes_icon.png")),
 					onPressed: () {},),
 		Text(movieDetails.getRatingFor(RATING_SOURCE.ROTTEN_TOMATOES)),
