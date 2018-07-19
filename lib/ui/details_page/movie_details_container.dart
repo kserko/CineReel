7cb4ba77588179a0d4bdb93ab4ea288f96e9b75c
serko <serko.apps@gmail.com>
2018-07-19 10:24:59 +0100

    - rename cast_widget to movie_details_cast_widget

--- lib/ui/details_page/movie_details_container.dart
+++ lib/ui/details_page/movie_details_container.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc_movies/bloc/movie_details_bloc.dart';
 import 'package:flutter_bloc_movies/models/tmdb_movie_details.dart';
 import 'package:flutter_bloc_movies/ui/common_widgets/common_widgets.dart';
 import 'package:flutter_bloc_movies/ui/common_widgets/movies_loading_widget.dart';
-import 'package:flutter_bloc_movies/ui/details_page/cast_widget.dart';
+import 'package:flutter_bloc_movies/ui/details_page/movie_details_cast_widget.dart';
 import 'package:flutter_bloc_movies/ui/details_page/ratings_widget.dart';
 
 class MovieDetailsContainer extends StatelessWidget {
