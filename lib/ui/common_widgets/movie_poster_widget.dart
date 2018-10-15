9040bbb52172cabe2ed43ceddfe9179c39a391f5
serko <serko.apps@protonmail.com>
2018-10-15 22:53:43 +0100

    - experimental transition when loading posters

--- lib/ui/common_widgets/movie_poster_widget.dart
+++ lib/ui/common_widgets/movie_poster_widget.dart
@@ -1,4 +1,5 @@
 import 'package:cine_reel/constants/api_constants.dart';
+import 'package:cine_reel/ui/common_widgets/common_widgets.dart';
 import 'package:cine_reel/utils/image_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -14,13 +15,23 @@ class MoviePosterWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-		return Hero(child: getFadeInImage(context), tag: "$id-imagePath");
+		return Hero(child: _buildImage(context), tag: "$id-imagePath");
   }
 
-	Widget getFadeInImage(BuildContext context) {
-		return FadeInImage.memoryNetwork(
+//	Widget getFadeInImage(BuildContext context) {
+//		return FadeInImage.memoryNetwork(
+//				placeholder: kTransparentImage,
+//				image: ImageHelper.getImagePath(imagePath, size),
+//				fit: BoxFit.fitWidth);
+//	}
+
+  _buildImage(BuildContext context) {
+		var fadedImage = FadeInImage.memoryNetwork(
 				placeholder: kTransparentImage,
 				image: ImageHelper.getImagePath(imagePath, size),
 				fit: BoxFit.fitWidth);
+
+  	return AnimateChildren(childOne: fadedImage, childTwo: SizedBox(width: 100.0), showHappyPath:
+		fadedImage != null);
 	}
 }
